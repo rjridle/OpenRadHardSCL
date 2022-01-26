31d6d3fdbb95962838bd8378251dbe803e6dbdda
rjridle <ryanridley46@gmail.com>
2022-01-25 19:34:58 -0600

    fixed magic extraction flow, added more xschem schematics

--- lib/magic/magic_calma.tcl
+++
@@ -1,7 +0,0 @@
-box values -35 0 -34 1
-select visible
-select more chunk
-select more chunk
-select visible
-box values [lindex [box values] 0] 0 [lindex [box values] 2] 1110
-property FIXED_BBOX [box values]
--- scripts/runlvs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-netgen -noc << EOF
-permute transistors
-lvs INVX1.sp INVX1_diff_ring.spice
-quit
-EOF
