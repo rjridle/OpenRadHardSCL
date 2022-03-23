02a28bb0d90fad2b9bc735ccaf64c5220de2cc04
rjridle <ryanridley46@gmail.com>
2022-03-23 15:29:43 -0500

    Flattened spice with correct dimensions for characterization

--- lib/scripts/magic_extract.tcl
+++ lib/scripts/magic_extract.tcl
@@ -1,17 +1,7 @@
 extract all
 
 # Hierarchical support unneeded.
-ext2spice subcircuit on
-ext2spice subcircuit top on
-ext2spice subcircuit descend on
-ext2spice hierarchy on
-ext2spice format ngspice
-ext2spice cthresh infinite
-ext2spice rthresh infinite
-ext2spice renumber off
-ext2spice merge aggressive
-ext2spice global on
-ext2spice -B -F -f ngspice -J flat
+ext2spice -m
 gds
 
 #ext2sim labels on
