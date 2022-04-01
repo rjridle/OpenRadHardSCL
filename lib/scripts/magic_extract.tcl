b120f7dcd40ab0f3be7e9cabbf7ac817ba20f4d9
rjridle <ryanridley46@gmail.com>
2022-04-01 14:14:09 -0500

    Update whole library

--- lib/scripts/magic_extract.tcl
+++ lib/scripts/magic_extract.tcl
@@ -3,12 +3,5 @@ extract all
 # Hierarchical support unneeded.
 ext2spice -m -t#
 gds
-
-#ext2sim labels on
-#ext2sim cthresh 0.0001
-#ext2sim -R -C
-#ext2sim -R -C -o tmp.sim
-
-#extresist tolerance 10
-#extresist all
-#ext2spice extresist on
+gds read $env(CELLNAME).gds
+lef writeall
